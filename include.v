61572b36612ec4c559e80089ab36848bdcf68533
Barkoum Betra Felix <felix_betran@yahoo.com>
2022-09-19 23:58:02 +0200

    Add files via upload

---
+++ include.v
@@ -0,0 +1,17 @@
+`define pretrained
+`define numLayers 4
+`define dataWidth 8
+`define numNeuronLayer1 30
+`define numWeightLayer1 784
+`define Layer1ActType "sigmoid"
+`define numNeuronLayer2 20
+`define numWeightLayer2 30
+`define Layer2ActType "sigmoid"
+`define numNeuronLayer3 10
+`define numWeightLayer3 20
+`define Layer3ActType "sigmoid"
+`define numNeuronLayer4 10
+`define numWeightLayer4 10
+`define Layer4ActType "hardmax"
+`define sigmoidSize 10
+`define weightIntWidth 4
